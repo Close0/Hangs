961e2e84f1dbb3d411489abc1c157b2bc63a9c2c
Sean Huitger <sean@codebysean.com>
2014-11-01 00:49:33 -0400

    The initial commit

---
+++ GameVoxHanging/HangingForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace GameVoxHanging
+{
+    partial class HangingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMemoryDump = new System.Windows.Forms.Button();
+            this.rtbConsoleOutput = new System.Windows.Forms.RichTextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.gbGameVoxStatus = new System.Windows.Forms.GroupBox();
+            this.rbGameVoxNotResponding = new System.Windows.Forms.RadioButton();
+            this.rbGameVoxOperational = new System.Windows.Forms.RadioButton();
+            this.gbGameVoxStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMemoryDump
+            // 
+            this.btnMemoryDump.Location = new System.Drawing.Point(219, 13);
+            this.btnMemoryDump.Name = "btnMemoryDump";
+            this.btnMemoryDump.Size = new System.Drawing.Size(141, 72);
+            this.btnMemoryDump.TabIndex = 0;
+            this.btnMemoryDump.Text = "Start diagnostics now";
+            this.btnMemoryDump.UseVisualStyleBackColor = true;
+            this.btnMemoryDump.Click += new System.EventHandler(this.btnMemoryDump_Click);
+            // 
+            // rtbConsoleOutput
+            // 
+            this.rtbConsoleOutput.Location = new System.Drawing.Point(13, 91);
+            this.rtbConsoleOutput.Name = "rtbConsoleOutput";
+            this.rtbConsoleOutput.Size = new System.Drawing.Size(347, 339);
+            this.rtbConsoleOutput.TabIndex = 1;
+            this.rtbConsoleOutput.Text = "";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(160, 13);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 2;
+            // 
+            // gbGameVoxStatus
+            // 
+            this.gbGameVoxStatus.Controls.Add(this.rbGameVoxOperational);
+            this.gbGameVoxStatus.Controls.Add(this.rbGameVoxNotResponding);
+            this.gbGameVoxStatus.Location = new System.Drawing.Point(13, 13);
+            this.gbGameVoxStatus.Name = "gbGameVoxStatus";
+            this.gbGameVoxStatus.Size = new System.Drawing.Size(200, 72);
+            this.gbGameVoxStatus.TabIndex = 3;
+            this.gbGameVoxStatus.TabStop = false;
+            this.gbGameVoxStatus.Text = "GameVox is";
+            // 
+            // rbGameVoxNotResponding
+            // 
+            this.rbGameVoxNotResponding.AutoSize = true;
+            this.rbGameVoxNotResponding.Checked = true;
+            this.rbGameVoxNotResponding.Location = new System.Drawing.Point(7, 20);
+            this.rbGameVoxNotResponding.Name = "rbGameVoxNotResponding";
+            this.rbGameVoxNotResponding.Size = new System.Drawing.Size(141, 17);
+            this.rbGameVoxNotResponding.TabIndex = 0;
+            this.rbGameVoxNotResponding.TabStop = true;
+            this.rbGameVoxNotResponding.Text = "currently not responding.";
+            this.rbGameVoxNotResponding.UseVisualStyleBackColor = true;
+            // 
+            // rbGameVoxOperational
+            // 
+            this.rbGameVoxOperational.AutoSize = true;
+            this.rbGameVoxOperational.Location = new System.Drawing.Point(7, 44);
+            this.rbGameVoxOperational.Name = "rbGameVoxOperational";
+            this.rbGameVoxOperational.Size = new System.Drawing.Size(173, 17);
+            this.rbGameVoxOperational.TabIndex = 1;
+            this.rbGameVoxOperational.Text = "working correctly, but will hang.";
+            this.rbGameVoxOperational.UseVisualStyleBackColor = true;
+            // 
+            // HangingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 443);
+            this.Controls.Add(this.gbGameVoxStatus);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.rtbConsoleOutput);
+            this.Controls.Add(this.btnMemoryDump);
+            this.Name = "HangingForm";
+            this.Text = "Form1";
+            this.gbGameVoxStatus.ResumeLayout(false);
+            this.gbGameVoxStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMemoryDump;
+        private System.Windows.Forms.RichTextBox rtbConsoleOutput;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.GroupBox gbGameVoxStatus;
+        private System.Windows.Forms.RadioButton rbGameVoxOperational;
+        private System.Windows.Forms.RadioButton rbGameVoxNotResponding;
+    }
+}
+
---
+++ GameVoxHanging/HangingForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameVoxHanging
+{
+    public partial class HangingForm : Form
+    {
+        private Process memoryDumpProcess;
+        public HangingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMemoryDump_Click(object sender, EventArgs e)
+        {
+            // Clear the old output from any previous runs
+            rtbConsoleOutput.Clear();
+
+            // Disable the button so people don't click it a bunch of times
+            btnMemoryDump.Enabled = false;
+
+            // Create a new Process object
+            memoryDumpProcess = new Process();
+            memoryDumpProcess.StartInfo.FileName = "procdump.exe";
+            memoryDumpProcess.StartInfo.Arguments = "-accepteula -ma GameVox.exe";
+            // Set UseShellExecute to false for redirection.
+            memoryDumpProcess.StartInfo.UseShellExecute = false;
+
+            // We don't want a command prompt window showing so hide it
+            memoryDumpProcess.StartInfo.CreateNoWindow = true;
+
+            // We need to raise this so we can listen for things like Exited
+            memoryDumpProcess.EnableRaisingEvents = true;
+
+            // Redirect the standard output of the sort command.   
+            // This stream is read asynchronously using an event handler.
+            memoryDumpProcess.StartInfo.RedirectStandardOutput = true;
+
+            // Set our event handler to asynchronously read the sort output.
+            memoryDumpProcess.OutputDataReceived += new DataReceivedEventHandler(MemoryDumpOutputHandler);
+            // Set our event handler to asynchronously exit when finished
+            memoryDumpProcess.Exited += new EventHandler(MemoryDumpProcessExited);
+
+            // Need to start the process before I can start the reading
+            memoryDumpProcess.Start();
+
+            // Start the asynchronous read of the sort output stream.
+            memoryDumpProcess.BeginOutputReadLine();
+        }
+
+        private void MemoryDumpProcessExited(object sender, EventArgs e)
+        {
+            rtbConsoleOutput.BeginInvoke(new MethodInvoker(delegate { btnMemoryDump.Enabled = true; }));
+            memoryDumpProcess.Dispose();
+        }
+
+        private void MemoryDumpOutputHandler(object sender, DataReceivedEventArgs output)
+        {
+            if (!String.IsNullOrEmpty(output.Data))
+            {
+                rtbConsoleOutput.BeginInvoke(new MethodInvoker(() => rtbConsoleOutput.AppendText(output.Data)));
+            }
+        }
+    }
+}
